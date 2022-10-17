517c39fe65a3ac423a45a35ae6eabfce69dafa24
Onur Erdogan <104835068+onureredo@users.noreply.github.com>
2022-10-17 12:19:01 +0200

    Update Nav.js

--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,6 @@
 import React, {useContext} from "react";
 import { MovieContext } from "./MovieContext";
 
-
 const Nav = () => {
     const [movies, setMovies] = useContext(MovieContext);
     return (
